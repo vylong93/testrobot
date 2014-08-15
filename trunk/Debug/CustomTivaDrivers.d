96ccbcc96f08841854ac07a905332d5b88a418ee
lebinhson90@gmail.com <lebinhson90@gmail.com@0117bdef-2823-debd-c0f3-0acc0c7438a7>
2014-08-15 07:54:53 +0000

    CHECK POINT

---
+++ trunk/Debug/CustomTivaDrivers.d
@@ -0,0 +1,58 @@
+CustomTivaDrivers.d: ../CustomTivaDrivers.c \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_ints.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_nvic.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_gpio.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_adc.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_udma.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_timer.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_ssi.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/adc.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/udma.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/timer.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h \
+ D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/debug.h \
+ ../CustomTivaDrivers.h
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_ints.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_nvic.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_gpio.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_adc.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_udma.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_timer.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/inc/hw_ssi.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/adc.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/udma.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/timer.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h:
+
+D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/debug.h:
+
+../CustomTivaDrivers.h:
---
+++ trunk/Debug/subdir_rules.mk
@@ -0,0 +1,13 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Compiler'
+	"D:/TI/ccsv6/tools/compiler/gcc-arm-none-eabi-4_7-2013q3/bin/arm-none-eabi-gcc.exe" -c -mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -DPART_TM4C123GH6PM -DTARGET_IS_BLIZZARD_RB1 -Dgcc -I"D:/TI/ccsv6/tools/compiler/gcc-arm-none-eabi-4_7-2013q3/arm-none-eabi/include" -I"E:/00 SwarmRobot/Workspace/Bootloader_Test" -I"D:/Program Files/TI/TivaWare_C_Series-2.1.0.12573" -O0 -ffunction-sections -fdata-sections -g -gstrict-dwarf -Wall -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
