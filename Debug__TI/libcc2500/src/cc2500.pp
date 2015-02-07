81ca2b34209eee2fb393e6c759ec5c64ccc0fa18
vylong93khtn@gmail.com <vylong93khtn@gmail.com@0117bdef-2823-debd-c0f3-0acc0c7438a7>
2015-02-07 11:21:16 +0000

    full project - CHECKPOINT

--- Debug__TI/libcc2500/src/cc2500.pp
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-libcc2500/src/cc2500.obj: ../libcc2500/src/cc2500.c
-
-../libcc2500/src/cc2500.c: 
--- Debug__TI/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"D:/Program\ Files/TI/TivaWare_C_Series-2.1.0.12573/driverlib/ccs/Debug/driverlib.lib" -l"D:/Program\ Files/TI/CMSIS-SP-00300-r4p0-00rel0/CMSIS/Lib/CCS/M4/dsplib-cm4f.lib"
-
